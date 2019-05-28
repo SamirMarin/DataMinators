a70369866e457b5cf1934894498fa8aba20b06f0
Chris Kwon <oskwon0504@gmail.com>
2019-05-28 09:29:42 -0700

    Added basepath

--- loadfile.py
+++ loadfile.py
@@ -1,8 +1,11 @@
+import os
 from pymongo import MongoClient
 from pprint import pprint
 import pandas as pd
 client = MongoClient("mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb")
 db=client.test
+basepath = os.path.expanduser('~/') + path
+
 employee = db.employee
 df = pd.read_csv("~/Desktop/yelp/yelp-dataset/yelp_user.csv")
 records_ = df.to_dict(orient = 'records')

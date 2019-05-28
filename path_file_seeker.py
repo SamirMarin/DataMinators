2491ebee9ca6551e7cdda166896f3e193dee4011
smarin <samir.marin@visier.com>
2019-05-28 02:22:42 -0700

    find files in path

--- path_file_seeker.py
+++ path_file_seeker.py
@@ -2,31 +2,30 @@ import os
 import pathlib
 basepath = os.path.expanduser('~') + '/esldata'
 count = 0 
-for dirpath, dirnames, files in os.walk(basepath):
-    if count <= 2:
-        print(dirpath.rfind('/'))
-        print(dirpath[dirpath.rfind('/')+1:len(dirpath)])
-        print(f'Found directory: {dirpath}')
-        print(dirnames)
-        print(files)
-    #for file_name in files:
-    #    print(file_name)
-    count += 1
+#for dirpath, dirnames, files in os.walk(basepath):
+#    if count <= 2:
+#        print(dirpath.rfind('/'))
+#        print(dirpath[dirpath.rfind('/')+1:len(dirpath)])
+#        print(f'Found directory: {dirpath}')
+#        print(dirnames)
+#        print(files)
+#    #for file_name in files:
+#    #    print(file_name)
+#    count += 1
 
 def get_map_of_files(path):
-    basepath = os.path.expanduser('~') + path
-    count = 0
+    basepath = os.path.expanduser('~/') + path
+
     for dirpath, dirnames, files in os.walk(basepath):
         current_dirname = get_dir_name(dirpath)
-        if count == 0:
-            print("name")
-        dirmap = {}
-        for dir_name in dirnames:
-            dirmap[dir_name] = []
-
-        for file_name in files:
-            print(file_name)
+        dirmap = {} 
+        dirmap['files'] = files
+        dirmap['dir'] = dirnames
+        return dirmap
+        
 
 
 def get_dir_name(dirpath):
     return print(dirpath[dirpath.rfind('/')+1:len(dirpath)])
+
+print(get_map_of_files(basepath))

f061bb4fcb6b6ab33c603adbba54c344d4cfaa5b
Chris Kwon <oskwon0504@gmail.com>
2019-05-28 11:19:36 -0700

    Updated loadFile script to take in parameters

--- loadfile.py
+++ loadfile.py
@@ -2,12 +2,13 @@ import os
 from pymongo import MongoClient
 from pprint import pprint
 import pandas as pd
-client = MongoClient("mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb")
-db=client.test
-basepath = os.path.expanduser('~/') + path
 
-employee = db.employee
-df = pd.read_csv("~/Desktop/yelp/yelp-dataset/yelp_user.csv")
-records_ = df.to_dict(orient = 'records')
-result = db.employee.insert_many(records_)
+def load_mongo_table(path, tableName):	
+	client = MongoClient("mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb")
+	db = client.test_3
+	table_name = db[tableName]
+	df = pd.read_csv(path)
+	records_ = df.to_dict(orient = 'records')
+	table_name.insert_many(records_)
 
+load_mongo_table("~/Desktop/yelp/yelp-dataset/yelp_tip.csv", "test_table")
